062200cd7d832e017c5596a955cdca0bd1a08474
lxz <326324917@qq.com>
2018-02-28 21:45:10 +0800

    include_all

--- sgs/client/MyCppGame/Classes/model/include_all.h
+++ sgs/client/MyCppGame/Classes/model/include_all.h
@@ -1,17 +1,20 @@
-#include "sgsgameattr.h"
-#include "player.h"
-#include "room.h"
-#include "proto.h"
-#include "../jsonproto/jsonproto.h"
-#include "../ppacket.h"
 #include <list>
 #include <map>
 #include <iostream>
 #include <stdio.h>
 #include <string>
-#include "cocos2d.h"
-#include "ui/CocosGUI.h"
-#include "ui\UIWidget.h"
+
 #include "boost.asio.1.0.159/boost/asio.hpp"
 #include "../Classes/libs/json/json.h"
 #include "../Classes/libs/json/json-forwards.h"
+#include "../ppacket.h"
+#include "ui/CocosGUI.h"
+#include "ui/UIWidget.h"
+#include "cocos2d.h"
+
+#include "../jsonproto/jsonproto.h"
+
+#include "sgsgameattr.h"
+#include "player.h"
+#include "room.h"
+#include "proto.h"
