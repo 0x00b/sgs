dcc807b5bee330464b1a615ac2cd30164a511ce4
lxz <326324917@qq.com>
2018-02-28 14:43:30 +0800

    appdelegate modify

--- sgs/client/MyCppGame/Classes/AppDelegate.cpp
+++ sgs/client/MyCppGame/Classes/AppDelegate.cpp
@@ -30,6 +30,8 @@ static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
+Player u_player();  //纪录用户信息
+
 //tcp::iostream AppDelegate::skt("10.14.115.244", "37373");
 //tcp::iostream AppDelegate::skt("10.12.137.251", "37373"); 
 //tcp::iostream AppDelegate::skt;
