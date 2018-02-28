02e03c6c4cf7ac6ab449d765b2ee5c0b1b9ca4c5
lxz <326324917@qq.com>
2018-02-28 23:10:10 +0800

    two part of do function

--- sgs/client/MyCppGame/Classes/Do_function.h
+++ sgs/client/MyCppGame/Classes/Do_function.h
@@ -10,7 +10,23 @@ public:
 	~Do_function();
 
 public:
+	static void PLAYER_REGIST_UC(Json::Value &pkt, int cmd);
 	static void PLAYER_LOGIN_UC(Json::Value &pkt, int cmd);
-
+	static void PLAYER_UPDATE_PWD_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_QUIT_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_GET_FRIENDS_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_DELETE_FRIENDS_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_ADD_FRIENDS_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_MATCH_ROOM_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_MATCH_ROOM_FAST_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_ENTER_ROOM_BC(Json::Value &pkt, int cmd);
+	static void PLAYER_QUIT_ROOM_BC(Json::Value &pkt, int cmd);
+	static void PLAYER_SEARCH_ROOM_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_READY_BC(Json::Value &pkt, int cmd);
+	static void PLAYER_SELECT_GAME_MODE_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_GET_GAME_MODE_UC(Json::Value &pkt, int cmd);
+	static void PLAYER_CREATE_ROOM_UC(Json::Value &pkt, int cmd);
 };
+
+
 
