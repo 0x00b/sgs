f990b6cf60cce5a85d40c33b28aad5faee7eb555
MM F <1509945419@qq.com>
2018-03-07 16:32:34 +0800

    ip

--- sgs/client/MyCppGame/Classes/AppDelegate.cpp
+++ sgs/client/MyCppGame/Classes/AppDelegate.cpp
@@ -39,7 +39,7 @@ Room u_room;
 
 
 tcp::socket sock(*(new boost::asio::io_service()));
-tcp::endpoint ep(boost::asio::ip::address::from_string("10.12.137.251"),37373);
+tcp::endpoint ep(boost::asio::ip::address::from_string("10.12.137.144"),37373);
 
 #include <stdio.h>
 #include <stdlib.h>
