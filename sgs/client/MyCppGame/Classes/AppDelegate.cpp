76ede3a80779992bd489731fdbfe6040d860d3a7
MM F <1509945419@qq.com>
2018-03-14 21:08:40 +0800

    改成宿舍ip 录屏用

--- sgs/client/MyCppGame/Classes/AppDelegate.cpp
+++ sgs/client/MyCppGame/Classes/AppDelegate.cpp
@@ -39,7 +39,7 @@ Room u_room;
 
 
 tcp::socket sock(*(new boost::asio::io_service()));  // 10.12.137.172
-tcp::endpoint ep(boost::asio::ip::address::from_string("10.12.137.194"),37373);
+tcp::endpoint ep(boost::asio::ip::address::from_string("10.14.115.244"),37373);
 
 #include <stdio.h>
 #include <stdlib.h>
